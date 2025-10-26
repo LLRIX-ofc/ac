b869e70752c119af9bfcf715eb2b815fc27d928c
LLRIX-ofc <llrix.ofc@gmail.com>
2025-10-27 00:26:50 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+require('./homekit-server');
+
+console.log('Starting Tuya AC HomeKit Bridge...');
+console.log('Make sure you have configured your .env file with:');
+console.log('- TUYA_ACCESS_ID');
+console.log('- TUYA_ACCESS_SECRET');
+console.log('- TUYA_DEVICE_ID');

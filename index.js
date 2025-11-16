495b2a14efc706ba3cc09335385925b408796bcb
LLRIX-ofc <llrix.ofc@gmail.com>
2025-11-16 16:57:40 +0300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-require('./homekit-server');
-
-console.log('Starting Tuya AC HomeKit Bridge...');
-console.log('Make sure you have configured your .env file with:');
-console.log('- TUYA_ACCESS_ID');
-console.log('- TUYA_ACCESS_SECRET');
-console.log('- TUYA_DEVICE_ID');
